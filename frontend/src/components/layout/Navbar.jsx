3eef0664222816c876adbd51f27e3768b9598aad
NguyenTuKien <KienNT.B23CN465@stu.ptit.edu.vn>
2026-03-05 11:59:40 +0700

    Update

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -4,15 +4,9 @@ import './Navbar.css';
 const Navbar = () => {
   return (
     <header className="navbar">
-      <div className="navbar-logo">
-        <div className="navbar-logo-icon">
-          <span className="material-symbols-outlined">school</span>
-        </div>
-        <span className="navbar-logo-text">PTIT Admin</span>
-      </div>
       <div className="navbar-search">
         <span className="material-symbols-outlined navbar-search-icon">search</span>
-        <input className="navbar-search-input" placeholder="Tìm kiếm kỳ thi, sinh viên, bảo các..." type="text" />
+        <input className="navbar-search-input" placeholder="Tìm kiếm tv, sinh viên, báo cáo..." type="text" />
       </div>
       <div className="navbar-actions">
         <button className="navbar-btn navbar-btn-primary">
@@ -25,12 +19,12 @@ const Navbar = () => {
         </button>
         <button className="navbar-notification">
           <span className="material-symbols-outlined">notifications</span>
-          <span className="navbar-notification-badge"></span>
+          <span className="navbar-notification-badge">17</span>
         </button>
         <div className="navbar-profile">
           <div className="navbar-profile-avatar" style={{ backgroundImage: 'url("https://lh3.googleusercontent.com/aida-public/AB6AXuBK3BIxgM3uAo44l0v-kMEiVYQgKYQ6mFAXqYj1wEuyZF_TS63cd7bzZPqf6LBlH3KbsIRgafo1YjHzRDY3CO1s3mapC2Txm_q_7aq-CNSUKUYpwctVm-CPp7nYsYWmtXLSaSCahtPLZc3_OKVPCNGOx646XaNnngLc-13lCVD1flRHaDX_Vuvbe_gUwToYHmF7oHXPtUDQrOE8g29trfhn5GDfz782s7zD18exrpHWWLPpC2DinKzezTR64YJOzIangkcSdjaDudc")' }}></div>
           <div className="navbar-profile-info">
-            <p className="navbar-profile-name">Admin, QTV</p>
+            <p className="navbar-profile-name">Admin_GTV</p>
             <p className="navbar-profile-role">Quản trị viên</p>
           </div>
           <span className="material-symbols-outlined navbar-profile-arrow">expand_more</span>
